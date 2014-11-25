79b73492adc385a1e68e2546cf0599f2ffc4243b
Philon <therealphilon@gmail.com>
2014-11-25 16:22:15 +0100

    windows realigned.

--- Assets/Scripts/Config/Generated/TrackedBundleVersion.cs
+++ Assets/Scripts/Config/Generated/TrackedBundleVersion.cs
@@ -6,14 +6,15 @@ public class TrackedBundleVersion
 
 	public static readonly TrackedBundleVersionInfo Version_1_15 =  new TrackedBundleVersionInfo ("1.15", 0);
 	public static readonly TrackedBundleVersionInfo Version_1_14 =  new TrackedBundleVersionInfo ("1.14", 1);
+	public static readonly TrackedBundleVersionInfo Version_1_16 =  new TrackedBundleVersionInfo ("1.16", 3);
 	
 	public ArrayList history = new ArrayList ();
 
-	public TrackedBundleVersionInfo current = new TrackedBundleVersionInfo ("1.15", 2);
+	public TrackedBundleVersionInfo current = new TrackedBundleVersionInfo ("1.16", 3);
 
 	public  TrackedBundleVersion() {
-		history.Add (Version_1_15);
 		history.Add (Version_1_14);
+		history.Add (Version_1_15);
 		history.Add (current);
 	}
 
